d805f9b168d1f2dc92994b82cb9a8511d4cfa63a
Jorge Soler <jorgeasoler2015@gmail.com>
2025-06-23 18:25:23 -0500

    feat: add Redis client, add moodle client

---
+++ educontrol/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Proyecto EduControl
+"""
+
+from src.settings import settings
+from src.utils.logging.logger_factory import setup_logging, get_logger
+
+
+def main():
+    setup_logging()
+
+
+
+if __name__ == "__main__":
+    main()
